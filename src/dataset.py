a020b98e18b438ef87ab94b78e4e2523ec1f50c3
Siyuan Wu <saberwu2002@gmail.com>
2025-03-05 21:15:40 +0000

    update dataset module

--- src/dataset.py
+++ src/dataset.py
@@ -13,6 +13,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from PIL import Image
 
 from conversation import get_conv_template
+from constant import *
 
 IGNORE_TOKEN_ID = LabelSmoother.ignore_index
 CONV_TEMPLATE_NAME = 'vicuna_v1.1'
@@ -34,6 +35,12 @@ class ModelArguments:
 class DataArguments:
     data_path: str = field(default=None, metadata={"help": "Path to the training data."})
     is_multimodal: bool = field(default=False)
+    has_video: bool = field(default=False)
+    has_image: bool = field(default=False)
+    has_audio: bool = field(default=False)
+    video_folder: str = field(default=None)
+    image_folder: str = field(default=None)
+    audio_folder: str = field(default=None)
 
 
 @dataclass
@@ -43,15 +50,96 @@ class TrainingArguments(transformers.TrainingArguments):
 
 def preprocess_multimodal(
     sources: List[Dict[str, str]],
-    data_args: DataArguments
+    data_args: DataArguments,
+    image_token_num=1,  # For number of image tokens
+    audio_token_num=1   # For number of audio tokens
 ) -> Dict:
+    """Prepare input token template with multimodal placeholder"""
     is_multimodal = data_args.is_multimodal
     if not is_multimodal:
         return sources
     
-    # TODO: Implement multimodal preprocessing
+    for source in sources:
+        for sentence in source:
+            if (DEFAULT_IMAGE_TOKEN in sentence['value'] or 
+                DEFAULT_VIDEO_TOKEN in sentence['value'] or 
+                DEFAULT_AUDIO_TOKEN in sentence['value']):
+                
+                # Image Tokens
+                sentence['value'] = sentence['value'].replace(
+                    DEFAULT_IMAGE_TOKEN + '\n', DEFAULT_IMAGE_TOKEN
+                ).strip()
+                sentence['value'] = sentence['value'].replace(
+                    '\n' + DEFAULT_IMAGE_TOKEN, DEFAULT_IMAGE_TOKEN
+                ).strip()
+                if sentence['value'].endswith(DEFAULT_IMAGE_TOKEN):
+                    IMAGE_TOKEN_NUM = sentence['value'].count(DEFAULT_IMAGE_TOKEN)
+                    sentence['value'] = sentence['value'].replace(
+                        DEFAULT_IMAGE_TOKEN * IMAGE_TOKEN_NUM, ''
+                    ).strip()
+                    sentence['value'] = DEFAULT_IMAGE_TOKEN * IMAGE_TOKEN_NUM + sentence['value']
+                    sentence['value'] = sentence['value'].strip()
+                
+                # Video Tokens
+                if sentence['value'].endswith(DEFAULT_VIDEO_TOKEN):
+                    VIDEO_TOKEN_NUM = sentence['value'].count(DEFAULT_VIDEO_TOKEN)
+                    sentence['value'] = sentence['value'].replace(
+                        DEFAULT_VIDEO_TOKEN * VIDEO_TOKEN_NUM, ''
+                    ).strip()
+                    sentence['value'] = DEFAULT_VIDEO_TOKEN * VIDEO_TOKEN_NUM + sentence['value']
+                    sentence['value'] = sentence['value'].strip()
+                
+                # Audio Tokens
+                sentence['value'] = sentence['value'].replace(
+                    DEFAULT_AUDIO_TOKEN + '\n', DEFAULT_AUDIO_TOKEN
+                ).strip()
+                sentence['value'] = sentence['value'].replace(
+                    '\n' + DEFAULT_AUDIO_TOKEN, DEFAULT_AUDIO_TOKEN
+                ).strip()
+                if sentence['value'].endswith(DEFAULT_AUDIO_TOKEN):
+                    AUDIO_TOKEN_NUM = sentence['value'].count(DEFAULT_AUDIO_TOKEN)
+                    sentence['value'] = sentence['value'].replace(
+                        DEFAULT_AUDIO_TOKEN * AUDIO_TOKEN_NUM, ''
+                    ).strip()
+                    sentence['value'] = DEFAULT_AUDIO_TOKEN * AUDIO_TOKEN_NUM + sentence['value']
+                    sentence['value'] = sentence['value'].strip()
+
+                # TODO: conversation
+                # if "mmtag" in conversation_lib.default_conversation.version:
+                #     sentence['value'] = sentence['value'].replace(
+                #         DEFAULT_IMAGE_TOKEN,
+                #         '<Image>' + DEFAULT_IMAGE_TOKEN + '</Image>'
+                #     )
+                
+                IMAGE_TOKEN_NUM = sentence['value'].count(DEFAULT_IMAGE_TOKEN)
+                if IMAGE_TOKEN_NUM > MAX_IMAGE_LENGTH:
+                    sentence['value'] = sentence['value'].replace(
+                        DEFAULT_IMAGE_TOKEN * IMAGE_TOKEN_NUM,
+                        DEFAULT_IMAGE_TOKEN * MAX_IMAGE_LENGTH
+                    ).strip()
+
+            # Prepare replacement tokens for image, video, and audio
+            replace_token = DEFAULT_IMAGE_TOKEN
+            vid_replace_token = DEFAULT_IMAGE_TOKEN * image_token_num
+            aud_replace_token = DEFAULT_AUDIO_TOKEN * audio_token_num
+            if data_args.mm_use_im_start_end:
+                replace_token = DEFAULT_IM_START_TOKEN + replace_token + DEFAULT_IM_END_TOKEN
+                vid_replace_token = DEFAULT_VID_START_TOKEN + vid_replace_token + DEFAULT_VID_END_TOKEN
+                aud_replace_token = DEFAULT_AUDIO_START_TOKEN + aud_replace_token + DEFAULT_AUDIO_END_TOKEN
+
+            sentence["value"] = sentence["value"].replace(
+                DEFAULT_IMAGE_TOKEN, replace_token + '\n'
+            )
+            sentence["value"] = sentence["value"].replace(
+                DEFAULT_VIDEO_TOKEN, vid_replace_token + '\n'
+            )
+            sentence["value"] = sentence["value"].replace(
+                DEFAULT_AUDIO_TOKEN, aud_replace_token + '\n'
+            )
+            sentence['value'] = sentence['value'].replace('\n\n', '\n')
     return sources
-    
+
+
 def preprocess(
     source: List[Dict[str, str]],
     tokenizer: transformers.PreTrainedTokenizer,
@@ -172,54 +260,61 @@ class LazySupervisedDataset(Dataset):
         raw_data = self.raw_data[idx]
         
         # FIXME: Currently there is no indicative token for multimodal data
+        # TODO: This should happens after the processing steps below since we need to know the length of image/audio dataset
         sources = preprocess_multimodal(
             copy.deepcopy(raw_data['conversations']),
             self.data_args
         )
         
-        # if 'image' in raw_data:
-        #     image_file_name = raw_data['image']
-        #     image_folder = self.data_args.image_folder
-        #     image_processor = self.data_args.image_processor
-        #     image = Image.open(os.path.join(image_folder, image_file_name)).convert('RGB')
+        # TODO: consider add a 'video' section that load video information, and returns the number of visual tokens
+        
+        if data_args.has_image:
+            assert 'images_folder' in raw_data, "Image folder not found in data"
             
-        #     # Padding and processing image
-        #     if self.data_args.image_aspect_ratio == 'pad':
-        #         def expand2square(pil_img, background_color):
-        #             width, height = pil_img.size
-        #             if width == height:
-        #                 return pil_img
-        #             elif width > height:
-        #                 result = Image.new(pil_img.mode, (width, width), background_color)
-        #                 result.paste(pil_img, (0, (width - height) // 2))
-        #                 return result
-        #             else:
-        #                 result = Image.new(pil_img.mode, (height, height), background_color)
-        #                 result.paste(pil_img, ((height - width) // 2, 0))
-        #                 return result
-        #         image = expand2square(image, tuple(int(x*255) for x in image_processor.image_mean))
-        #         image = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
-        #     else:
-        #         image = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            image_file_name = raw_data['images_folder']
+            image_folder = self.data_args.image_folder
+            image_processor = self.data_args.image_processor
+            image = Image.open(os.path.join(image_folder, image_file_name)).convert('RGB')
+            
+            # Padding and processing image
+            if self.data_args.image_aspect_ratio == 'pad':
+                def expand2square(pil_img, background_color):
+                    width, height = pil_img.size
+                    if width == height:
+                        return pil_img
+                    elif width > height:
+                        result = Image.new(pil_img.mode, (width, width), background_color)
+                        result.paste(pil_img, (0, (width - height) // 2))
+                        return result
+                    else:
+                        result = Image.new(pil_img.mode, (height, height), background_color)
+                        result.paste(pil_img, ((height - width) // 2, 0))
+                        return result
+                image = expand2square(image, tuple(int(x*255) for x in image_processor.image_mean))
+                image = image_processor.preprocess(image, return_tensors='pt')
+            else:
+                image = image_processor.preprocess(image, return_tensors='pt')
                 
-        # if 'audio' in sources:
-        #     audio_file_name = sources['audio']
-        #     audio_folder = self.data_args.audio_folder
-        #     audio_processor = self.data_args.audio_processor
-        #     audio_file_name = os.path.join(audio_folder, audio_file_name)
-        #     audio = audio_processor.get_hidden_states(audio_file_name)
+        if data_args.has_audio:
+            assert 'audio_file' in raw_data, "Audio file not found in data"
+            
+            audio_file_name = raw_data['audio_file']
+            audio_folder = self.data_args.audio_folder
+            audio_processor = self.data_args.audio_processor
+            audio_file_name = os.path.join(audio_folder, audio_file_name)
+            audio = audio_processor.get_hidden_states(audio_file_name)
         
         data_dict = preprocess(
             sources,
             self.tokenizer,
-            has_image='image' in sources,
-            has_audio='audio' in sources
+            has_image=data_args.has_image,
+            has_audio=data_args.has_audio
         )
         
-        # if 'image' in raw_data:
-        #     data_dict['image'] = image
-        # if 'audio' in raw_data:
-        #     data_dict['audio'] = audio
+        if data_args.has_image:
+            data_dict['image'] = image
+        if data_args.has_audio:
+            data_dict['audio'] = audio
             
         return data_dict
         
@@ -244,6 +339,10 @@ if __name__ == "__main__":
             "--model_name_or_path", "/home/saberwu2002/CS229-Project/hf_ckp/vicuna-7b-v1.5",
             "--data_path", "/home/saberwu2002/CS229-Project/local_data/MMTrail_processed/test/metas_video_convs.json",
             "--output_dir", "/home/saberwu2002/CS229-Project/output/",
+            "--has_video", "--has_image", "--has_audio",
+            "--video_folder", "/home/saberwu2002/CS229-Project/local_data/MMTrail_processed/test/videos/",
+            "--image_folder", "/home/saberwu2002/CS229-Project/local_data/MMTrail_processed/test/images/",
+            "--audio_folder", "/home/saberwu2002/CS229-Project/local_data/MMTrail_processed/test/audio/"
         ]
     )
     
