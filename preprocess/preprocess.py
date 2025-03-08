9ae4929af7fdd236668faafbac1cb047379d8a2b
Tonyzhang2000 <zhangshutong2000@gmail.com>
2025-03-07 20:42:08 -0500

    update preprocess script

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -78,7 +78,7 @@ def preprocess_MMTrail_metadata(dataset_root_dir: str):
             # process audio metadata
             meta_processed_audio = {
                 'id': meta['video_id'],
-                'video_path': meta['original_metadata']['video_path'],
+                # 'video_path': meta['original_metadata']['video_path'],
                 'images_folder': meta['images_folder'],
                 'audio_file': meta['audio_file'],
                 'conversations': [
@@ -92,7 +92,24 @@ def preprocess_MMTrail_metadata(dataset_root_dir: str):
                     }
                 ]
             }
-            return meta_processed_video, meta_processed_audio
+
+            meta_processed_video_audio = {
+                'id': meta['video_id'],
+                'video_path': meta['original_metadata']['video_path'],
+                'images_folder': meta['images_folder'],
+                'audio_file': meta['audio_file'],
+                'conversations': [
+                    {
+                        'from': 'human',
+                        'value': "<audio>\n<video>\n" + random.choice(audio_questions) # TODO: change this to video + audio question
+                    },
+                    {
+                        'from': 'assistant',
+                        'value': meta['original_metadata']['music_caption'][0]['text']
+                    }
+                ]
+            }
+            return meta_processed_video, meta_processed_audio, meta_processed_video_audio
         
         except Exception as e:
             print(f'Error processing metadata: {meta["video_id"]}')
@@ -106,6 +123,7 @@ def preprocess_MMTrail_metadata(dataset_root_dir: str):
         metas_dir = os.path.join(split_dir, 'metas')
         metas_processed_video_fp = os.path.join(split_dir, 'metas_video_convs.json')
         metas_processed_audio_fp = os.path.join(split_dir, 'metas_audio_convs.json')
+        metas_processed_video_audio_fp = os.path.join(split_dir, 'metas_video_audio_convs.json')
         if not os.path.exists(metas_dir):
             print(f'No metadata found for [{split}] split.')
             continue
@@ -114,22 +132,27 @@ def preprocess_MMTrail_metadata(dataset_root_dir: str):
         # process each meta file
         metas_processed_video_list = []
         metas_processed_audio_list = []
+        metas_processed_video_audio_list = []
         for meta_file in os.listdir(metas_dir):
             with open(os.path.join(metas_dir, meta_file), 'r') as f:
                 meta = json.load(f)
                 
             # preprocess metadata and save to corresponding directory
-            meta_processed_video, meta_processed_audio = preprocess(meta)
+            meta_processed_video, meta_processed_audio, meta_processed_video_audio = preprocess(meta)
             if meta_processed_video is not None:
                 metas_processed_video_list.append(meta_processed_video)
             if meta_processed_audio is not None:
                 metas_processed_audio_list.append(meta_processed_audio)
+            if meta_processed_video_audio is not None:
+                metas_processed_video_audio_list.append(meta_processed_video_audio)
                 
         # save processed metadata
         with open(metas_processed_video_fp, 'w') as f:
             json.dump(metas_processed_video_list, f, indent=4)
         with open(metas_processed_audio_fp, 'w') as f:
             json.dump(metas_processed_audio_list, f, indent=4)
+        with open(metas_processed_video_audio_fp, 'w') as f:
+            json.dump(metas_processed_video_audio_list, f, indent=4)
             
     print('Preprocessing completed.')
     
